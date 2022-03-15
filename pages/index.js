7e0a9705de520975133e8a86b42565cd204410f9
Patrick Sparks <pto1026@gmail.com>
2022-03-15 04:28:38 -0600

    finished header section

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,40 @@ export default function Home() {
         <h1 className="heading-primary u-margin-bottom-48">
           Build beautiful, high-retaining websites with Elite.
         </h1>
-        <ButtonPrimary href="#">Buy now</ButtonPrimary>
+        <div className="header__button">
+          <ButtonPrimary href="#">Buy now</ButtonPrimary>
+        </div>
       </div>
+      <div className="home-top-divider">
+        <img
+          className="home-top-divider__image container"
+          src="imgs/top-image.jpg"
+          alt="Photo of technology and photography items"
+        />
+      </div>
+      <main>
+        <section className="about container">
+          <div className="about__left">
+            <img
+              className="about__image"
+              src="imgs/about-image.jpg"
+              alt="Picture of keyboard representing prgramming"
+            />
+          </div>
+          <div className="about__box u-text-white">
+            <h2 className="heading-secondary u-margin-bottom-20">
+              CSS files included
+            </h2>
+            <h3 className="heading-tertiary u-margin-bottom-30">
+              Get the CSS files, all included with customizable options.
+            </h3>
+            <p className="about__paragraph">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+              enim laboriosam ratione doloribus fuga quis asperiores?
+            </p>
+          </div>
+        </section>
+      </main>
     </React.Fragment>
   );
 }
