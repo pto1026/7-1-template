6de05eb71ca72412f10d016ec2912f3876b5dfc0
Patrick Sparks <pto1026@gmail.com>
2022-03-17 16:20:09 -0600

    implemented faq dropdown functionality

--- components/faq.js
+++ components/faq.js
@@ -3,17 +3,17 @@ import Question from './question';
 export default function Faq() {
   return (
     <div className="faq u-margin-bottom-120">
-      <Question>What kind of data can I find with Elite?</Question>
+      <Question answer="I do everything, everything.">What kind of data can I find with Elite?</Question>
 
-      <Question>What makes Elite different from other tools?</Question>
+      <Question answer="I do everything, everything.">What makes Elite different from other tools?</Question>
 
-      <Question>How do you take payments?</Question>
+      <Question answer="I do everything, everything.">How do you take payments?</Question>
 
-      <Question>Do you believe in the moon, the planets, and gravity?</Question>
+      <Question answer="I do everything, everything.">Do you believe in the moon, the planets, and gravity?</Question>
 
-      <Question>What&apos; your refund and cancellation policy?</Question>
+      <Question answer="I do everything, everything.">What&apos; your refund and cancellation policy?</Question>
 
-      <Question>Can I track web analytics with Elite?</Question>
+      <Question answer="I do everything, everything.">Can I track web analytics with Elite?</Question>
     </div>
   );
 }
--- components/question.js
+++ components/question.js
@@ -1,12 +1,29 @@
-export default function Question({ children }) {
-  return (
-    <div className="question">
-      <p className="question__text">{children}</p>
-      <img
-        className="question__icon"
-        src="icons/caret-down.svg"
-        alt="Caret-down icon"
-      />
-    </div>
-  );
+import { useState } from "react";
+
+export default function Question({ answer, children }) {
+  const [open, setOpen] = useState(0);
+  if (open === 0) {
+    return (
+      <div className="question" onClick={() => setOpen(1)}>
+        <p className="question__text">{children}</p>
+        <img
+          className="question__icon"
+          src="icons/caret-down.svg"
+          alt="Caret-down icon"
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="question" onClick={() => setOpen(0)}>
+        <p className="question__text">{children}</p>
+        <img
+          className="question__icon"
+          src="icons/caret-down.svg"
+          alt="Caret-down icon"
+        />
+        <p className="question__answer">{answer}</p>
+      </div>
+    );
+  }
 }
