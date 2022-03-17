f2e75037d3971909fe151c85d7fc7f90e8b6796c
Patrick Sparks <pto1026@gmail.com>
2022-03-17 01:12:20 -0600

    more work

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ import CustomerStory from "../components/customer-story";
 import Reviews from "../components/reviews";
 import PricingCards from "../components/pricing-cards";
 import Question from "../components/question";
-import Faq from '../components/faq';
+import Faq from "../components/faq";
 
 export default function Home() {
   return (
@@ -180,7 +180,60 @@ export default function Home() {
             <h3 className="heading-tertiary">Read latest blogs</h3>
           </header>
 
-          
+          <div className="resources">
+            <figure className="blog-post">
+              <img
+                className="blog-post__image u-margin-bottom-48"
+                src="imgs/suit-man-resized.jpg"
+                alt="Man in a red suit"
+              />
+              <figcaption className="blog-post__text">
+                <h4 className="heading-four u-margin-bottom-32">
+                  The Art of Reading Customers
+                </h4>
+                <p className="blog-post__paragraph">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Error soluta iure fugit quibusdam assumenda quaerat illo eos
+                  doloremque! Minima laudantium eum magnam odio odit recusandae.
+                </p>
+              </figcaption>
+            </figure>
+            <figure className="blog-post">
+              <img
+                className="blog-post__image u-margin-bottom-48"
+                src="imgs/bookshelf-resized.jpg"
+                alt="Bookshelf"
+              />
+              <figcaption className="blog-post__text">
+                <h4 className="heading-four u-margin-bottom-32">
+                  Fix the one mistake everyone makes
+                </h4>
+                <p className="blog-post__paragraph">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                  molestias assumenda itaque in recusandae a accusamus dolores
+                  nostrum placeat inventore repudiandae earum sint, quo
+                  laudantium quia dolorum nesciunt porro veritatis!
+                </p>
+              </figcaption>
+            </figure>
+            <figure className="blog-post">
+              <img
+                className="blog-post__image u-margin-bottom-48"
+                src="imgs/whiteboard-presentation-resized.jpg"
+                alt="Whiteboard presentation"
+              />
+              <figcaption className="blog-post__text">
+                <h4 className="heading-four u-margin-bottom-32">
+                  Best tools in the industry
+                </h4>
+                <p className="blog-post__paragraph">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Temporibus mollitia reiciendis nemo nesciunt, aspernatur, vero
+                  obcaecati odio omnis.
+                </p>
+              </figcaption>
+            </figure>
+          </div>
         </section>
       </main>
     </React.Fragment>
