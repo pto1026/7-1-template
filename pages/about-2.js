ea93fe7c0e4204ad98b8ac220feaaa8be1723106
Patrick Sparks <pto1026@gmail.com>
2022-03-27 10:56:31 -0600

    finished some styling on cta section

---
+++ pages/about-2.js
@@ -0,0 +1,25 @@
+export default function About2() {
+  return (
+    <div>
+      <main>
+        <div className="header container u-text-center">
+          <div className="u-text-main">
+            <h2 className="heading-secondary u-margin-bottom-32">
+              About the company
+            </h2>
+          </div>
+          <h1 className="heading-primary u-margin-bottom-48">
+            We built Elite for teams that take their product growth seriously.
+          </h1>
+          <p className="header__paragraph u-margin-bottom-96">
+            Most tools are just a lot of noise. Well we&apos;re about action,
+            see for yourself &rarr;
+          </p>
+          <div className="header__image-box">
+              <img className="header__image" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,11 @@
+import React from 'react'
+
 export default function About() {
-  return (
-    <div>
-      <main>
-        <div className="header container u-text-center">
-          <div className="u-text-main">
-            <h2 className="heading-secondary u-margin-bottom-32">
-              About the company
-            </h2>
-          </div>
-          <h1 className="heading-primary u-margin-bottom-48">
-            We built Elite for teams that take their product growth seriously.
-          </h1>
-          <p className="header__paragraph u-margin-bottom-96">
-            Most tools are just a lot of noise. Well we&apos;re about action,
-            see for yourself &rarr;
-          </p>
-          <div className="header__image-box">
-              <img className="header__image" />
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <main>
+                <header className=""
+            </main>
+        </React.Fragment>
+    )
 }
