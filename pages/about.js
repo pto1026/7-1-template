e2549d11a63b109eb971ea87d1ed14cc7855e2dd
pto1026 <92680192+pto1026@users.noreply.github.com>
2022-06-14 20:20:19 -0600

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,6 @@ export default function About() {
     return (
         <React.Fragment>
             <main>
-                <header className=""
             </main>
         </React.Fragment>
     )
